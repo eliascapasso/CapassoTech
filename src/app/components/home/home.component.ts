44c5db453d9b43d471fa64bf1a64b97800dfd6aa
elias_capasso <elias_capasso@live.com>
2024-08-06 12:59:51 -0300

    servicios modificados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { getLang } from 'src/app/helpers/helper';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,12 @@ import { getLang } from 'src/app/helpers/helper';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, public router: Router) {
     translate.setDefaultLang(getLang(translate.getBrowserLang()!));
     translate.use(getLang(translate.getBrowserLang()));
+  }
+
+  goToPage(url: string) {
+    this.router.navigateByUrl(url);
   }
 }
